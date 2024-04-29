4ac527e9cd08b651bd786088b501664cc8ad3d96
Carol-77 <carolnjagi77@gmail.com>
2024-04-29 14:35:57 +0300

    Added API integration

--- src/weather.js
+++ src/weather.js
@@ -1,9 +1,26 @@
+   function changeWeather(response){
+      let temperatureElement = document.querySelector(#temperature);
+      temperatureElement.innerHTML = response.data.temperature.current;
+      temperatureElement.innerHTML = Math.round(temperature);
+   }
+   
+   
+   
+   
+   function searchCity(city){
+    let apiKey = "a33b693cfbefd271b0ed075f9a8f65f0";
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${a33b693cfbefd271b0ed075f9a8f65f0}&units=metric';
+    axios.get(apiUrl).then(changeWeather);
+   
+   
+   
    
    function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#city-input");
     let cityElement = document.querySelector("#city-name");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
    } 
 
     let cityInputElement = document.querySelector("#search-form");
